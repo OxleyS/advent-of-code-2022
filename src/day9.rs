76d0a0f995379a661f3c625153bffb88c7e8b93e
unknown <13091705+OxleyS@users.noreply.github.com>
2022-12-10 03:41:54 +0900

    Optimize and clean up day 9

--- src/day9.rs
+++ src/day9.rs
@@ -15,18 +15,21 @@ fn solve_impl<const N: usize>() -> usize {
 
     for line in iterate_file_lines("day9input.txt") {
         let (direction, num_steps) = line.split_once(' ').expect("Malformed line");
+        let direction = direction.as_bytes()[0]; // Guaranteed ASCII
         let num_steps = num_steps.parse::<usize>().expect("Malformed step count");
 
-        // TODO: Optimize this to be branchless?
+        // By reducing the direction test from a string to a u8, we make it likely
+        // that this match compiles to a lookup table instead of 4 string comparisons
         let move_amt = match direction {
-            "L" => (-1i32, 0i32),
-            "R" => (1i32, 0i32),
-            "U" => (0i32, -1i32),
-            "D" => (0i32, 1i32),
+            76 => (-1i32, 0i32), // L
+            82 => (1i32, 0i32),  // R
+            85 => (0i32, -1i32), // U
+            68 => (0i32, 1i32),  // D
             _ => panic!("Malformed direction"),
         };
 
         for _ in 0..num_steps {
+            // Move the head knot
             knots[0].x += move_amt.0;
             knots[0].y += move_amt.1;
 
@@ -43,10 +46,15 @@ fn solve_impl<const N: usize>() -> usize {
                     continue;
                 }
 
+                // Signum gives us the "direction" to move in.
+                // If the front knot is on the same row/column, one of these will
+                // be zero, and the back knot only moves one space.
+                // Otherwise, the back knot moves in both X and Y (that is, diagonally)
                 back.x += delta.x.signum();
                 back.y += delta.y.signum();
             }
 
+            // Mark where the tail ended up
             visited.insert(knots[N - 1]);
         }
     }
