5d7ac7e247f29628b44e74d36db550a192e25426
unknown <13091705+OxleyS@users.noreply.github.com>
2022-12-18 03:31:11 +0900

    [WIP] Bad Day 16 Part 2

--- src/day16.rs
+++ src/day16.rs
@@ -16,11 +16,18 @@ struct Valve {
 
 pub fn solve() {
     let (valves, start_idx) = parse_valves();
+
     let start_time = Instant::now();
     let max_pressure = solve_part1(&valves, start_idx);
     let time_taken = Instant::elapsed(&start_time);
-    println!("Max releasable pressure is {max_pressure}");
-    println!("Took {} seconds", time_taken.as_secs_f32());
+    println!("Max releasable pressure alone is {max_pressure}");
+    println!("This took {} seconds", time_taken.as_secs_f32());
+
+    let start_time = Instant::now();
+    let max_pressure = solve_part2(&valves, start_idx);
+    let time_taken = Instant::elapsed(&start_time);
+    println!("Max releasable pressure with elephant is {max_pressure}");
+    println!("This took {} seconds", time_taken.as_secs_f32());
 }
 
 fn solve_part1(valves: &[Valve], start_idx: usize) -> usize {
@@ -32,12 +39,13 @@ fn solve_part1(valves: &[Valve], start_idx: usize) -> usize {
         prev_valve: usize,
         minutes: usize,
         valves_open: &mut [bool],
+        num_open: usize,
     ) -> usize {
         let valve = &valves[cur_valve];
         let cur_released = cur_released + release_rate;
-        if minutes == 1 {
-            // One minute left, nothing else to do
-            return cur_released;
+        if minutes == 1 || num_open == valves.len() {
+            // Nothing else to do
+            return cur_released + (release_rate * (minutes - 1));
         }
 
         // They can go to another valve, but no point doubling back without doing anything
@@ -54,13 +62,14 @@ fn solve_part1(valves: &[Valve], start_idx: usize) -> usize {
                     cur_valve,
                     minutes - 1,
                     valves_open,
+                    num_open,
                 )
             })
             .max()
             .unwrap_or(cur_released);
 
-        // If there's no point opening this valve, or we can't, don't bother
-        if valve.flow_rate == 0 || valves_open[cur_valve] {
+        // If we can't open the valve, stop here
+        if valves_open[cur_valve] {
             return best_tunnel;
         }
 
@@ -73,16 +82,171 @@ fn solve_part1(valves: &[Valve], start_idx: usize) -> usize {
             cur_valve,
             minutes - 1,
             valves_open,
+            num_open + 1,
         );
         valves_open[cur_valve] = false;
         best_if_opened.max(best_tunnel)
     }
 
-    recurse(valves, 0, 0, start_idx, start_idx, 30, &mut vec![false; valves.len()])
+    let mut num_open = 0;
+    let mut valves_open = vec![false; valves.len()];
+    for (i, valve) in valves.iter().enumerate() {
+        if valve.flow_rate == 0 {
+            valves_open[i] = true;
+            num_open += 1;
+        }
+    }
+
+    recurse(valves, 0, 0, start_idx, start_idx, 30, &mut valves_open, num_open)
+}
+
+fn solve_part2(valves: &[Valve], start_idx: usize) -> usize {
+    fn recurse_human(
+        valves: &[Valve],
+        cur_released: usize,
+        release_rate: usize,
+        cur_human: usize,
+        prev_human: usize,
+        cur_elephant: usize,
+        prev_elephant: usize,
+        minutes: usize,
+        valves_open: &mut [bool],
+        num_open: usize,
+    ) -> usize {
+        let valve = &valves[cur_human];
+        let cur_released = cur_released + release_rate;
+        if minutes == 1 || num_open == valves.len() {
+            // Nothing else to do
+            return cur_released + (release_rate * (minutes - 1));
+        }
+
+        // They can go to another valve, but no point doubling back without doing anything
+        let best_tunnel = valve
+            .tunnels
+            .iter()
+            .filter(|&&idx| idx != prev_human)
+            .map(|idx| {
+                recurse_elephant(
+                    valves,
+                    cur_released,
+                    release_rate,
+                    *idx,
+                    cur_human,
+                    cur_elephant,
+                    prev_elephant,
+                    minutes,
+                    valves_open,
+                    num_open,
+                )
+            })
+            .max()
+            .unwrap_or(cur_released);
+
+        // If we can't open the valve, stop here
+        if valves_open[cur_human] {
+            return best_tunnel;
+        }
+
+        valves_open[cur_human] = true;
+        let best_if_opened = recurse_elephant(
+            valves,
+            cur_released,
+            release_rate + valve.flow_rate,
+            cur_human,
+            cur_human,
+            cur_elephant,
+            prev_elephant,
+            minutes,
+            valves_open,
+            num_open + 1,
+        );
+        valves_open[cur_human] = false;
+        best_if_opened.max(best_tunnel)
+    }
+
+    fn recurse_elephant(
+        valves: &[Valve],
+        cur_released: usize,
+        release_rate: usize,
+        cur_human: usize,
+        prev_human: usize,
+        cur_elephant: usize,
+        prev_elephant: usize,
+        minutes: usize,
+        valves_open: &mut [bool],
+        num_open: usize,
+    ) -> usize {
+        let valve = &valves[cur_elephant];
+
+        // They can go to another valve, but no point doubling back without doing anything
+        let best_tunnel = valve
+            .tunnels
+            .iter()
+            .filter(|&&idx| idx != prev_elephant)
+            .map(|idx| {
+                recurse_human(
+                    valves,
+                    cur_released,
+                    release_rate,
+                    cur_human,
+                    prev_human,
+                    *idx,
+                    cur_elephant,
+                    minutes - 1,
+                    valves_open,
+                    num_open,
+                )
+            })
+            .max()
+            .unwrap_or(cur_released);
+
+        // If we can't open the valve, stop here
+        if valves_open[cur_elephant] {
+            return best_tunnel;
+        }
+
+        valves_open[cur_elephant] = true;
+        let best_if_opened = recurse_human(
+            valves,
+            cur_released,
+            release_rate + valve.flow_rate,
+            cur_human,
+            prev_human,
+            cur_elephant,
+            cur_elephant,
+            minutes - 1,
+            valves_open,
+            num_open + 1,
+        );
+        valves_open[cur_elephant] = false;
+        best_if_opened.max(best_tunnel)
+    }
+
+    let mut num_open = 0;
+    let mut valves_open = vec![false; valves.len()];
+    for (i, valve) in valves.iter().enumerate() {
+        if valve.flow_rate == 0 {
+            valves_open[i] = true;
+            num_open += 1;
+        }
+    }
+
+    recurse_human(
+        valves,
+        0,
+        0,
+        start_idx,
+        start_idx,
+        start_idx,
+        start_idx,
+        26,
+        &mut valves_open,
+        num_open,
+    )
 }
 
 fn parse_valves() -> (Vec<Valve>, usize) {
-    let unresolved_valves: Vec<UnresolvedValve> = iterate_file_lines("day16input.txt")
+    let unresolved_valves: Vec<UnresolvedValve> = iterate_file_lines("testinput.txt")
         .map(|line| {
             let (valve_str, tunnels_str) = line.split_once(';').expect("Malformed line");
             let (start, flow_rate_str) = valve_str.split_once('=').expect("Malformed flow section");
